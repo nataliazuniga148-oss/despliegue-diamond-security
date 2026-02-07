61b60ab8cc7e77fdd34ea6effbb267c8eb4bba47
Natalia Zuñiga Hernández <nataliazuniga148@gmail.com>
2026-02-07 11:42:31 -0500

    Primer commit del proyecto

---
+++ diamond/urls.py
@@ -0,0 +1,47 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.principal, name='principal'),
+    path('logeo', views.logeo, name='logeo'),
+    path('acceso', views.acceso, name='acceso'),
+    path('contacto', views.contacto, name='contacto'),
+    path('registro', views.registro, name='registro'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('integraciones', views.integraciones, name='integraciones'),
+    path('recuperar', views.recuperar, name='recuperar'),
+    path('cotizar', views.cotizar, name='cotizar'),
+    path('admin', views.admin, name='admin'),
+    path('horarioad', views.horarioad, name='horarioad'),
+    path('informes', views.informes, name='informes'),
+    path('registroa', views.registroa, name='registroa'),
+    path('ingresousu', views.ingresousu, name='ingresousu'),
+    path('verusuario', views.verusuario, name='verusuario'),
+    path('eliminar', views.eliminar, name='eliminar'),
+    path('verdatosu', views.verdatosu, name='verdatosu'),
+    path('gestion', views.gestion, name='gestion'),
+    path('usuarios', views.usuarios, name='usuarios'),
+    path('ajustes', views.ajustes, name='ajustes'),
+    path('editarh', views.editarh, name='editarh'),
+    path('cliente', views.cliente, name='cliente'),
+    path('tarjetas', views.tarjetas, name='tarjetas'),
+    path('control', views.control, name='control'),
+    path('editart', views.editart, name='editart'),
+    path('buscar', views.buscar, name='buscar'),
+    path('editaracc', views.editaracc, name='editaracc'),
+    path('peatones', views.peatones, name='peatones'),
+    path('emergencia', views.emergencia, name='emergencia'),
+    path('discapacitados', views.discapacitados, name='discapacitados'),
+    path('vehiculos', views.vehiculos, name='vehiculos'),
+    path('recepcion', views.recepcion, name='recepcion'),
+    path('puertas', views.puertas, name='puertas'),
+    path('paquetes', views.paquetes, name='paquetes'),
+    path('visitantes', views.visitantes, name='visitantes'),
+    path('editarvisi', views.editarvisi, name='editarvisi'),
+    path('editarpa', views.editarpa, name='editarpa'),
+    path('configuracion', views.configuracion, name='configuracion'),
+    path('editarvehi', views.editarvehi, name='editarvehi'),
+    
+    
+]
+
---
+++ diamond/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render
+
+def principal(request):
+    return render(request, 'dashboard/principal.html')
+
+def logeo(request):
+    return render(request, 'usuarios/logeo.html')
+
+def acceso(request):
+    return render(request, 'acceso/acceso.html')
+
+def contacto(request):
+    return render(request, 'contacto/contacto.html')
+
+def registro(request):
+    return render(request, 'registro/registro.html')
+
+def nosotros(request):
+    return render(request, 'nosotros/nosotros.html')
+
+def integraciones(request):
+    return render(request, 'integraciones/integraciones.html')
+
+def recuperar(request):
+    return render(request, 'recuperar/recuperar.html')
+
+def cotizar(request):
+    return render(request, 'cotizar/cotizar.html')
+
+def admin(request):
+    return render(request, 'admin/admin.html')
+
+def horarioad(request):
+    return render(request, 'horarioad/horarioad.html')
+
+def informes(request):
+    return render(request, 'informes/informes.html')
+
+def registroa(request):
+    return render(request, 'registroa/registroa.html')
+
+def ingresousu(request):
+    return render(request, 'ingresousu/ingresousu.html')
+
+def verusuario(request):
+    return render(request, 'verusuario/verusuario.html')
+
+def eliminar(request):
+    return render(request, 'eliminar/eliminar.html')
+
+def verdatosu(request):
+    return render(request, 'verdatosu/verdatosu.html')
+
+def gestion(request):
+    return render(request, 'gestion/gestion.html')
+
+def gestion(request):
+    return render(request, 'gestion/gestion.html')
+
+def usuarios(request):
+    return render(request, 'usuarioshistorial/usuarios.html')
+
+def ajustes(request):
+    return render(request, 'ajustes/ajustes.html')
+
+def editarh(request):
+    return render(request, 'editarh/editarh.html')
+
+def cliente(request):
+    return render(request, 'cliente/cliente.html')
+
+def tarjetas(request):
+    return render(request, 'tarjetas/tarjetas.html')
+
+def control(request):
+    return render(request, 'control/control.html')
+
+def editart(request):
+    return render(request, 'editart/editart.html')
+
+def buscar(request):
+    return render(request, 'buscar/buscar.html')
+
+def editaracc(request):
+    return render(request, 'editaracc/editaracc.html')
+
+def peatones(request):
+    return render(request, 'peatones/peatones.html')
+
+def emergencia(request):
+    return render(request, 'emergencia/emergencia.html')
+
+def discapacitados(request):
+    return render(request, 'discapacitados/discapacitados.html')
+
+def vehiculos(request):
+    return render(request, 'vehiculos/vehiculos.html')
+
+def recepcion(request):
+    return render(request, 'recepcion/recepcion.html')
+
+def puertas(request):
+    return render(request, 'puertas/puertas.html')
+
+def paquetes(request):
+    return render(request, 'paquetes/paquetes.html')
+
+def visitantes(request):
+    return render(request, 'visitantes/visitantes.html')
+
+def editarvisi(request):
+    return render(request, 'editarvisi/editarvisi.html')
+
+def editarpa(request):
+    return render(request, 'editarpa/editarpa.html')
+
+def configuracion(request):
+    return render(request, 'configuracion/configuracion.html')
+
+def editarvehi(request):
+    return render(request, 'editarvehi/editarvehi.html')
+
+
+
