3bdc35626ff4d52f46c29011eacc46edbc728b9f
Natalia Zuñiga Hernández <nataliazuniga148@gmail.com>
2026-06-22 18:25:51 -0500

    fix project name inconsistency

--- programa/urls.py
+++ programa/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for diamond_security project.
+URL configuration for programa project
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- programa/wsgi.py
+++ programa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for diamond_security project.
+WSGI config for programa project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
